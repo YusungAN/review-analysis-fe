e7a052d996209d4f6160fe5c6460da6b0261ef7f
yusungan <dksdbtjd1@gmail.com>
2024-12-20 22:37:07 +0900

    fix: TrendChart.js chart.js 로드안되는 거 해결

--- src/components/TrendChart/TrendChart.tsx
+++ src/components/TrendChart/TrendChart.tsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react';
-// import {
-//     Chart as ChartJS,
-//     CategoryScale,
-//     LinearScale,
-//     PointElement,
-//     LineElement,
-//     Title,
-//     Tooltip,
-//     Legend,
-// } from 'chart.js';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
-// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const options = {
     responsive: true,
